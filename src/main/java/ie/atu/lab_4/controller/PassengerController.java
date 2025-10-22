f1567bce40724ff8cd5b0d721afae44eb5f1cb99
G00421777@atu.ie <G00421777@atu.ie>
2025-10-22 15:05:06 +0100

    addded passenger controller

---
+++ src/main/java/ie/atu/lab_4/controller/PassengerController.java
@@ -0,0 +1,43 @@
+package ie.atu.lab_4.controller;
+
+import ie.atu.lab_4.model.Passenger;
+import ie.atu.lab_4.service.PassengerService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/passenger")
+public class PassengerController {
+    private final PassengerService service;
+    public PassengerController(PassengerService service){
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Passenger>> getAll(){
+        return ResponseEntity.ok(service.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Passenger> getOne(@PathVariable String id) {
+        Optional<Passenger> maybe = service.findById(id);
+        if(maybe.isPresent()) {
+            return ResponseEntity.ok(maybe.get());
+        }else{
+            return  ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Passenger> create(@Valid @RequestBody Passenger p){
+        Passenger created = service.create(p);
+        return ResponseEntity
+                .created(URI.create("/api/passengers" + created.getPassengerId()))
+                .body(created);
+    }
+}
